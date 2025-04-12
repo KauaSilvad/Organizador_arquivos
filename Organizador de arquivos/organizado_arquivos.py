11598863befa91448e94df0dad06fd4bb2d017cc
Kauã Silva <166737683+KauaSilvad@users.noreply.github.com>
2025-04-12 09:23:52 -0300

    Add files via upload

---
+++ Organizador de arquivos/organizado_arquivos.py
@@ -0,0 +1,23 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+print(caminho)
+
+lista_arquivos = os.listdir(caminho)
+print(lista_arquivos)
+
+locals = {
+    "imagens": [".png", ".jpg", ".jpeg"],
+    "Planinhas":[".xlsx"],
+    "Pdfs":[".pdf"],
+    "csv": [".csv"],
+}
+
+for arquivo in lista_arquivos:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locals:
+        if extensao in locals[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
